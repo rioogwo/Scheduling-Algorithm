0763fd2fb8c34a8507f16b4bc202ec6964d9fafd
rioogwo <95230613+rioogwo@users.noreply.github.com>
2021-12-03 00:07:02 +0800

    Update SchedulingAlgorithm.c

--- SchedulingAlgorithm.c
+++ SchedulingAlgorithm.c
@@ -194,3 +194,62 @@ void showTable(int jobNumber, int AT[], int BT[], int priority[]){
         }
     }
 }
+
+
+//function name: SJN //
+//Code:Rio Giovanni Marie //
+
+#include <stdio.h>
+#include <conio.h>
+
+// MAIN FUNCTION//
+int main() 
+{
+//VARIABLES//
+      int arrival_time[10], burst_time[10], temp[10];
+      int i, smallest, count = 0, time, limit;
+      double wait_time = 0, turnaround_time = 0, end;
+      float average_waiting_time, average_turnaround_time;
+      printf("\nEnter the Total Number of Processes:");
+      scanf("%d", &limit); 
+      printf("\nEnter Details of %d Processesn", limit);
+      for(i = 0; i < limit; i++)
+      {
+      	
+//CREATING MENU//
+        printf("\nEnter Arrival Time:");
+        scanf("%d", &arrival_time[i]);
+        printf("Enter Burst Time:");
+        scanf("%d", &burst_time[i]); 
+        temp[i] = burst_time[i];
+      }
+      burst_time[9] = 9999;  
+      for(time = 0; count != limit; time++)
+      {
+            smallest = 9;
+            for(i = 0; i < limit; i++)
+            {
+                  if(arrival_time[i] <= time && burst_time[i] < burst_time[smallest] && burst_time[i] > 0)
+                  {
+                        smallest = i;
+                  }
+            }
+        burst_time[smallest]--;
+        if(burst_time[smallest] == 0)
+            {
+                  count++;
+                  end = time + 1;
+                  wait_time = wait_time + end - arrival_time[smallest] - temp[smallest];
+                  turnaround_time = turnaround_time + end - arrival_time[smallest];
+            }
+      }
+      average_waiting_time = wait_time / limit; 
+      average_turnaround_time = turnaround_time / limit;
+      printf("\nAverage Waiting Time:%lfn", average_waiting_time);
+      printf("\nAverage Turnaround Time:%lfn", average_turnaround_time);
+
+//EXIT MESSAGE//
+      
+    printf("\nThank You For Using The Program!");
+      return 0;
+}
